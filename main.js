a157514292c96dd64cfb8d8faf55c652471c5343
blydiet <lydietoussaint123@gmail.com>
2020-03-18 15:53:55 -0400

    (featue-react-code)-I am refactoring the code to make it cleaner

--- main.js
+++ main.js
@@ -16,21 +16,47 @@ const hashValue = (str, hash) => {
     return hash
 }
 
+const mapString = (firstStr, secondStr) => {
+    if (firstStr.length !== secondStr.length){
+        console.log('false')
+        return false
+    }
+    let arrayStr1 = Object.values(hashValue(firstStr, {})).sort((a,b) => a - b)
+    let arrayStr2 = Object.values(hashValue(secondStr, {})).sort((a,b) => a - b)
+    let strPtr1 = 0
+    let strPtr2 = 0
+    let totalMapVal = 0
 
+    while ( strPtr1< arrayStr1.length && strPtr2 < arrayStr2.length){
+        console.log('array1',arrayStr1[strPtr1])
+        console.log('array2', arrayStr2[strPtr2])
+        if (arrayStr1[strPtr1] > arrayStr2[strPtr2]){
+            console.log('false')
+            return false
+        } else if (arrayStr1[strPtr1] < arrayStr2[strPtr2]){
+            totalMapVal += arrayStr2[strPtr1]
+            strPtr1++
+        } else if (totalMapVal === arrayStr2[strPtr2]) {
+            totalMapVal = 0
+            strPtr2++
+        } else {
+            strPtr1++
+            strPtr2++
+        }
+    }
+    console.log('true')
+    return true
+}
 prompt.get(['str1', 'str2'], (err, result) => {
     if (err) return onError(err)
     let firstStr = result.str1
     let secondStr = result.str2
-    if (firstStr.length !== secondStr.length){
-        return false
-    }
-    let hashStr1 = hashValue(firstStr, {})
-    let hashStr2 = hashValue(secondStr, {})
-    console.log(hashStr1)
-
+    mapString(firstStr, secondStr)
+    
 })
 
 const onError = (err)=> {
     console.log(err)
     return 1
 }
+
